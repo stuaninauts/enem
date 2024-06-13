9368bc37a32cdea3a13f53db5a41304bf9d10781
Eduardo Stuani <eduardostuani.cs@gmail.com>
2024-06-13 13:30:56 -0300

    refactoring app, first commit of changes

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
-
 import pandas as pd
 from shiny import App, Inputs, Outputs, Session, render, ui, reactive
+import faicons as fa
+import plotly.express as px
+from shiny import App, reactive, render, ui
+from shinywidgets import output_widget, render_plotly
+
+ICONS = {
+    "user": fa.icon_svg("user", "regular"),
+    "wallet": fa.icon_svg("wallet"),
+    "currency-dollar": fa.icon_svg("dollar-sign"),
+    "ellipsis": fa.icon_svg("ellipsis"),
+}
 
 # am for acertos e melhoras
 df_am = pd.read_csv("enem_acertos_melhoras.csv")
@@ -38,9 +48,9 @@ dict_questoes = {
 }
 
 # given color and factor, darken this color with this factor
-def darken_color(cor, fator):
+def darken_color(cor, factor):
     color_rgb = mcolors.to_rgb(cor)
-    new_color_rgb = tuple(comp * (1 - fator) for comp in color_rgb)
+    new_color_rgb = tuple(comp * (1 - factor) for comp in color_rgb)
     new_color = mcolors.to_hex(new_color_rgb)
     return new_color
 
@@ -56,7 +66,7 @@ def show_checkbox_areas():
         ),
         ui.input_checkbox(
             id="checkbox_acertos", 
-            label="Comparar acertos da primeira tentativa", 
+            label="Comparar acertos da primeira tentativa",
             value=False,
         ),
 
@@ -66,7 +76,7 @@ def show_checkbox_areas():
 def nav_acertos_melhoras():
     return [
         ui.layout_sidebar(
-            ui.panel_sidebar(
+            ui.sidebar(
                 ui.input_radio_buttons(
                     id="radio_ordem_am", 
                     label="Ordem das edições:", 
@@ -80,67 +90,154 @@ def nav_acertos_melhoras():
                     selected='1',
                 ),
                 ui.navset_hidden(
-                    ui.nav(None, "", value='0'),
-                    ui.nav(None, show_checkbox_areas(), value='1'),
+                    ui.nav_panel(None, "", value='0'),
+                    ui.nav_panel(None, show_checkbox_areas(), value='1'),
                     id="nav_radio_area",
                 ),    
             ),
-            ui.panel_main(
-                ui.output_plot("plot_am"),
-                # TOOO
-                # add details about the plot
+            ui.layout_columns(
+                ui.value_box(
+                    "Simulados Realizados", ui.output_ui("total_edicoes"), showcase=ICONS["user"]
+                ),
+                ui.value_box(
+                    "Acertos / Total", ui.output_ui("acertos_total"), showcase=ICONS["wallet"]
+                ),
+                ui.value_box(
+                    "Média de acertos por área",
+                    ui.output_ui("media_acertos_area"),
+                    showcase=ICONS["currency-dollar"],
+                ),
+            fill=False,
             ),
-        ),
+            ui.layout_columns(
+                ui.card(
+                    ui.card_header("Dados"), ui.output_data_frame("am_table"), full_screen=True
+                ),
+                ui.card(
+                    ui.card_header(
+                        "Quantidade de acertos vs edicao",
+                        ui.popover(
+                            ICONS["ellipsis"],
+                            ui.input_radio_buttons(
+                                "scatter_color",
+                                None,
+                                ["none", "sex", "smoker", "day", "time"],
+                                inline=True,
+                            ),
+                            title="Add a color variable",
+                            placement="top",
+                        ),
+                        class_="d-flex justify-content-between align-items-center",
+                    ),
+                    output_widget("scatterplot"),
+                    full_screen=True,
+                ),
+                ui.layout_columns(
+                     ui.value_box(
+                        "Média de melhora", ui.output_ui("media_melhora"), showcase=ICONS["wallet"]
+                    ),
+                    ui.card(
+                        ui.card_header(
+                            "Diferença entre acertos da primeira tentativa vs segunda tentativa",
+                            ui.popover(
+                                ICONS["ellipsis"],
+                                ui.input_radio_buttons(
+                                    "tip_perc_y",
+                                    "Split by:",
+                                    ["sex", "smoker", "day", "time"],
+                                    selected="day",
+                                    inline=True,
+                                ),
+                                title="Add a color variable",
+                            ),
+                            class_="d-flex justify-content-between align-items-center",
+                        ),
+                        output_widget("tip_perc"),
+                        full_screen=True,
+                    ),
+                    col_widths=[3, 9]
+                ),  
+                col_widths=[6, 6, 12],
+            ),
+
+        )
     ]
 
 # nav that show conversao de conhecimento related to df_cc
-def nav_conversao_conhecimento():
-    return [
-        # TODO
-        # explain ling and context
-        ui.layout_sidebar(
-            ui.panel_sidebar(
-                ui.input_radio_buttons(
-                    id="radio_ordem_cc", 
-                    label="Ordem das edições:", 
-                    choices={'0': "Alfabética", '1': "Cronológica por resolução"},
-                    selected='0',
-                ), 
-                ui.input_checkbox_group(
-                    id="checkbox_areas_cc",
-                    label="Escolher área:",
-                    # list choices using dict_areas from 1-3 index
-                    choices={chave: valor[0] for chave, valor in dict_areas.items() if list(dict_areas.keys()).index(chave) > 0}, 
-                    selected=list(dict_areas.keys()),
-                ),
-                ui.input_checkbox_group(
-                    id="checkbox_questoes_cc",
-                    label="Escolher tipos de questões:",
-                    choices=dict_questoes, 
-                    selected=list(dict_questoes.keys()),
-                ),    
+# def nav_conversao_conhecimento():
+#     return [
+#         # TODO
+#         # explain ling and context
+#         ui.layout_sidebar(
+#             ui.panel_sidebar(
+#                 ui.input_radio_buttons(
+#                     id="radio_ordem_cc", 
+#                     label="Ordem das edições:", 
+#                     choices={'0': "Alfabética", '1': "Cronológica por resolução"},
+#                     selected='0',
+#                 ), 
+#                 ui.input_checkbox_group(
+#                     id="checkbox_areas_cc",
+#                     label="Escolher área:",
+#                     # list choices using dict_areas from 1-3 index
+#                     choices={chave: valor[0] for chave, valor in dict_areas.items() if list(dict_areas.keys()).index(chave) > 0}, 
+#                     selected=list(dict_areas.keys()),
+#                 ),
+#                 ui.input_checkbox_group(
+#                     id="checkbox_questoes_cc",
+#                     label="Escolher tipos de questões:",
+#                     choices=dict_questoes, 
+#                     selected=list(dict_questoes.keys()),
+#                 ),    
 
-            ),
-            ui.panel_main(
-                ui.output_plot("plot_cc"),
-                # TODO
-                # add details like acertos de questoes que eu nao sabia
-            ),
-        ),
+#             ),
+#             ui.panel_main(
+#                 ui.output_plot("plot_cc"),
+#                 # TODO
+#                 # add details like acertos de questoes que eu nao sabia
+#             ),
+#         ),
 
-    ]
+#     ]
 
 app_ui = ui.page_fluid(
     # TODO
     # add title
-    # add context about everything
-    ui.navset_tab_card(
-        ui.nav("Análise quantidade de acertos e melhoras", nav_acertos_melhoras()),
-        ui.nav("Análise conversão de conhecimento", nav_conversao_conhecimento()),
+    # add tooltips ordem das edicoes, comparar acertos primeira tentativa
+    ui.navset_card_tab(
+        ui.nav_panel("Análise quantidade de acertos e melhorass", nav_acertos_melhoras()),
+        #ui.nav_panel("Análise conversão de conhecimento", nav_conversao_conhecimento()),
     ),
 )
 
 def server(input, output, session):
+    # INFORMATION CARDS
+    # gets the data from the acertos e melhoras section
+    @reactive.calc
+    def am_data():
+        return df_am
+
+    @render.ui
+    def total_edicoes():
+        return am_data().shape[0]
+
+
+    @render.ui
+    def acertos_total():
+        return 
+
+    @render.ui
+    def media_acertos_area():
+        return 
+
+    @render.ui
+    def media_melhor():
+        return 
+    
+    @render.data_frame
+    def am_table():
+        return render.DataGrid(am_data())
+    
     @reactive.Effect
     @reactive.event(input.radio_area)
     # expand the options if redacao is not choosen
@@ -218,7 +315,7 @@ def server(input, output, session):
         # global params for every plot
         ax.tick_params(axis='x', labelrotation=55, bottom=True)
         ax.grid(axis='both', color='0.75')
-        ax.set_title('titulo')        
+        #ax.set_title('titulo')        
         ax.set_xlabel('edicao')
 
         return fig
@@ -229,8 +326,8 @@ def server(input, output, session):
 
         result = df_cc
         
+        result = result.set_index('edicao')
         if input.radio_ordem_cc() == '0':
-            result = result.set_index('edicao')
             result = result.sort_index()
 
         # select the wanted columns and drop nan values
@@ -246,17 +343,12 @@ def server(input, output, session):
             for tipo_questao in list(input.checkbox_questoes_cc()):
                 # combine the selected areas with the type of question
                 # result the final columns to plot
-                colunas_selecionadas.append(coluna for coluna in colunas_area if coluna.endswith('_' + tipo_questao)) # modify generator
-        
+                colunas_selecionadas.append([coluna for coluna in colunas_area if coluna.endswith('_' + tipo_questao)])
         x = result.index # result['edicao']
         for coluna in colunas_selecionadas:
-            y = result[coluna] 
-            ax.plot(x, y, marker='o')  
-
-            
-
-
-        ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
+            y = result[coluna]
+            ax.plot(x, y, label=dict_areas[area][0],marker='o')  
+            ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
     
         ax.set_ylabel('questões')
         ax.set_yticks(range(20, 46, 2)) # modify later min and max
@@ -264,7 +356,7 @@ def server(input, output, session):
 
         ax.tick_params(axis='x', labelrotation=55, bottom=True)
         ax.grid(axis='both', color='0.75')
-        ax.set_title('titulo')        
+        #ax.set_title('titulo')        
         ax.set_xlabel('edicao')      
 
         return fig
@@ -278,5 +370,6 @@ def server(input, output, session):
     @render.plot
     def plot_cc():
         build_plot_cc()
+
 
 app = App(app_ui, server)
